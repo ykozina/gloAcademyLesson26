2e357c51b509f842e827c499fd3c5eeab492f2a6
Julia Kozina <ykozina@yandex.ru>
2025-04-30 00:36:38 +0300

    removed commented code

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 'use strict'
 
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//     method: 'POST',
-//     body: JSON.stringify({
-//         title: 'foo',
-//         body: 'bar',
-//         userId: 1,
-//     }),
-//     headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//     },
-// })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
-
 const getData = () => {
     return fetch('./db.json')
         .then(response => {
